ee4360bf5761106e88daab1b7d99363b049eb389
Patrick <phunanon@users.noreply.github.com>
2021-03-09 22:48:04 +0000

    Group into tables and add a heatmap

--- coinmarketcap-com-monitor.js
+++ coinmarketcap-com-monitor.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Coinmarketcap.com monitor
-// @version      0.5
+// @version      0.6
 // @author       Patrick Bowen
 // @match        https://coinmarketcap.com/all/views/all/
 // ==/UserScript==
@@ -8,10 +8,13 @@
 const secondsWaitIfSome = 10 * 60;
 const secondsWaitIfNone = 1 * 60;
 const numberOfCoins = 500;
+const groupSize = 100;
+const minMinutesSample = 5;
 
 
 const e = sel => document.querySelector(sel);
 const es = sel => [...document.querySelectorAll(sel)];
+const hex = n => (n < 0x10 ? "0" : "") + n.toString(16);
 
 function displayReport (text) {
     let report = e("report");
@@ -19,9 +22,9 @@ function displayReport (text) {
         const r = document.createElement("report");
         const styles = ["padding: 1rem", "position: fixed", "backgroundColor: #fff",
                         "border: .2rem solid #000", "zIndex: 1000", "width: 100%",
-                        "height: 45%", "bottom: 0", "overflowY: scroll", "fontSize: 1.1rem",
+                        "height: 45%", "bottom: 0", "overflow: auto", "fontSize: 1.1rem",
                         "lineHeight: 1.5rem"];
-        styles.map(s => s.split(": ")).forEach(([s, v]) => {r.style[s] = v; });
+        styles.map(s => s.split(": ")).forEach(([s, v]) => { r.style[s] = v; });
         document.body.appendChild(r);
         report = r;
     }
@@ -38,7 +41,7 @@ function waitUntil (when, then) {
 
 function getCoins () {
     const coins = es("td.cmc-table__cell--sort-by__symbol").map(n => n.innerText);
-    return [...new Set(coins)].slice(0, numberOfCoins);
+    return [...new Set(coins)].slice(0, numberOfCoins - 1); //-1 to prevent a table group of just #500
 }
 
 function getTime () {
@@ -52,7 +55,7 @@ function stage1 () {
     waitUntil(() => {
         const numCoins = getCoins().length;
         displayReport(`Version ${GM_info.script.version}<br>Loading coins... ${numCoins}/${numberOfCoins}`);
-        return numCoins >= numberOfCoins;
+        return numCoins >= numberOfCoins - 1;
     }, stage2);
 }
 
@@ -64,25 +67,59 @@ function stage2 () {
     const oldTime = localStorage.getItem("time");
     const newCoins = getCoins();
     const newTime = getTime();
-    localStorage.setItem("coins", newCoins.join(" "));
-    localStorage.setItem("time", newTime);
     const timeDiff = newTime - oldTime;
+    //Mitigate overwrite with young sample
+    if (timeDiff > 1000 * 60 * minMinutesSample) {
+        localStorage.setItem("coins", newCoins.join(" "));
+        localStorage.setItem("time", newTime);
+    }
     const oldCoins = oldCoinsTxt ? oldCoinsTxt.split(" ") : newCoins;
 
     //Find distances
     let dists = [];
-    for (const c in newCoins) {
+    for (let c in newCoins) {
+        c = parseInt(c);
         const prevIdx = oldCoins.indexOf(newCoins[c]);
         if (prevIdx == -1) continue;
-        dists.push([newCoins[c], prevIdx - c, prevIdx, c]);
+        dists.push({coin: newCoins[c], dist: prevIdx - c, prevI: prevIdx + 1, currI: c + 1});
     }
-    dists.sort((a, b) => b[1] - a[1]);
+    dists.sort((a, b) => b.dist - a.dist);
     stage3(dists, timeDiff);
 }
 
+function generateTable (dists, n) {
+    const rangeA = dists[0] * groupSize;
+    dists = dists[1];
+    return `<table class="coinReport" style="border-collapse: collapse; position: absolute; left: ${n * 16}rem; width: 15rem;">
+    <tr><th colspan="3">#${(rangeA ? rangeA : 1)} - #${rangeA + groupSize - 1} (${dists.length})</th></tr>
+    <tr><th>Coin</th><th>±</th><th>Position</th></tr>
+    <tr>${dists.map(d => `
+    <td>
+        <a href="https://uk.tradingview.com/chart/?symbol=${d.coin}BTC" target="_blank">${d.coin}</a>
+    </td>
+    <td>${d.dist}</td>
+    <td>#${d.prevI} ⇨ #${d.currI}</td>`).join("</tr><tr>")}</tr>
+    </table>`;
+}
+
+function groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+        const key = keyGetter(item);
+        const collection = map.get(key);
+        if (!collection) {
+            map.set(key, [item]);
+        } else {
+            collection.push(item);
+        }
+    });
+    return map;
+}
+
+
 //Create a report and put it onto the page
 function stage3 (dists, timeDiff) {
-    dists = dists.filter(d => d[1] != 0);
+    dists = dists.filter(d => d.dist != 0);
     const refreshSecs = (dists.length ? secondsWaitIfSome : secondsWaitIfNone);
     const numMinDiff = Math.round(timeDiff / 1000 / 60);
     let numMinRefr = Math.ceil(refreshSecs / 60);
@@ -90,15 +127,10 @@ function stage3 (dists, timeDiff) {
                       difference of ${numMinDiff} minute${(numMinDiff == 1 ? "" : "s")}.
                       Refreshing in <span id="refreshIn"></span>.
     <br><br>
-    <table id="coinReport">
-    <tr><th>Coin</th><th>Distance</th><th>Position</th></tr>
-    <tr>${dists.map(([c, d, oi, ni]) => `
-    <td>
-        <a href="https://uk.tradingview.com/chart/?symbol=${c}BTC" target="_blank">${c}</a>
-    </td>
-    <td>${d}</td>
-    <td>#${oi} &#8594; #${ni}</td>`).join("</tr><tr>")}</tr>
-    </table>`;
+    ${[...groupBy(dists, d => parseInt(d.currI / groupSize)).entries()]
+       .sort((a, b) => a[0] - b[0])
+       .map(generateTable)
+       .join("")}`;
     displayReport(reportText);
     //Update "refreshing in ..." text
     const updateRefreshingIn = () => {
@@ -107,10 +139,19 @@ function stage3 (dists, timeDiff) {
     };
     updateRefreshingIn();
     setInterval(updateRefreshingIn, 60000);
-    //Add table padding
-    es("#coinReport td, #coinReport th").forEach(t => {
+    //Add table cell styles
+    es(".coinReport td, .coinReport th").forEach(t => {
         t.style.padding = ".2rem";
         t.style.border = ".1rem solid #aaa";
+    });
+    //Add heatmaps
+    es(".coinReport tr")
+     .filter(t => t.children[0].tagName.toLowerCase() == "td")
+     .forEach(t => {
+        const n = parseInt(t.children[1].innerText);
+        const r = parseInt(Math.min(n < 0 ? -n * 1.5 : 0, 200));
+        const g = parseInt(Math.min(n > 0 ? n * 1.5 : 0, 200));
+        t.style.backgroundColor = `#${hex(255 - g)}${hex(255 - r)}${hex(255 - (r + g))}`;
     });
     //Set refresh
     setTimeout(() => window.location.reload(), refreshSecs * 1000);
