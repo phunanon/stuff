98504995f356b8b046928843896e1f9d393686da
Patrick <phunanon@users.noreply.github.com>
2021-04-28 14:02:40 +0100

    Update mtc.js

--- mtc.js
+++ mtc.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Timecamp time totter
-// @version      0.0.0.2
+// @version      0.0.0.3
 // @author       Patrick
 // @match        https://enterprise2.timecamp.com/app
 // @grant        none
@@ -10,6 +10,5 @@ setTimeout(() => {
     if (location.hash != "#/timesheets/timer") {
         return;
     }
-    alert("I'm here");
-    [...document.querySelectorAll(".tc-duration-picker .form-control:not([disabled])")].forEach(e => {e.style.fontSize = "2rem"; e.style.backgroundColor = "red";});
-}, 5000);
+    [...document.querySelectorAll("div.form-inline .tc-duration-picker .form-control:not([disabled])")].forEach(e => {e.style.fontSize = "2rem"; e.style.backgroundColor = "red";});
+}, 3000);
