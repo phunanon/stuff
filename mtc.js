a852c972aae1d8379d47e7114e0ad899049b6362
Patrick <phunanon@users.noreply.github.com>
2021-05-06 10:05:27 +0100

    Update mtc.js

--- mtc.js
+++ mtc.js
@@ -1,14 +1,35 @@
 // ==UserScript==
 // @name         Timecamp time totter
-// @version      0.0.0.3
+// @version      0.0.1
 // @author       Patrick
 // @match        https://enterprise2.timecamp.com/app
 // @grant        none
 // ==/UserScript==
+//http*://*/*
+
+function fmtSecs(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    return [hours, minutes, seconds]
+        .map((n, i) => ({ n, s: "hms"[i] }))
+        .filter(({ n }) => n > 0)
+        .map(({ n, s }) => n + s)
+        .join(" ");
+}
 
 setTimeout(() => {
+    console.log(`Loading ${GM_info.script.name} version ${GM_info.script.version}`);
     if (location.hash != "#/timesheets/timer") {
         return;
     }
-    [...document.querySelectorAll("div.form-inline .tc-duration-picker .form-control:not([disabled])")].forEach(e => {e.style.fontSize = "2rem"; e.style.backgroundColor = "red";});
+    //E.g. "0:07:19 h"
+    const seconds = [...document.querySelectorAll("div.form-inline .tc-duration-picker .form-control:not([disabled])")]
+        .map(e => e.value)
+        .map(txt => txt.split(" ")[0].split(":"))
+        .map(parts => parts.map(p => parseInt(p)))
+        .map(([h, m, s]) => (h * 60 * 60) + (m * 60) + s)
+        .reduce((a, b) => a + b, 0);
+    alert(fmtSecs(seconds));
 }, 3000);
